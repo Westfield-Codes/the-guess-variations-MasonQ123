ba00f8c1d1c90d5d5f043d785bf1e98f4eb4bba8
Mason Quinn <mason117167@student-schoolsofwestfield.org>
2025-10-01 13:55:20 +0000

    3 is broken

--- pseudocode/done.js
+++ pseudocode/done.js
@@ -1,1 +1,65 @@
+/* 0.75-guess-simple.js */
+function guessGame(){
+let guess = 0;
 
+let answer = Math.floor(Math.random()*100+1);
+
+guess = prompt("Guess a number, 1-100!")
+
+if(guess == answer){
+    alert("Correct!");
+}
+else if(guess > answer){
+alert("Too high!");
+}
+else{
+    alert("Too low!");
+}
+};
+
+/* 1-guess-recursive */
+let guess = 0
+let answer = Math.foor(Math.random()*100+1);
+function guessGame (){
+    while(guess != answer){
+guess = prompt("Guess a number. 1-100")
+if(guess == answer){
+    alert("Correct!")
+}
+else if(guess > answer){
+    alert("Too high!")
+}
+else{
+    alert("Too low!")
+}
+};
+};
+
+/* 2-guess-loop.js */
+function guessGame (){
+
+answer = Math.floor(Math.random()*100+1);
+
+let guess = 0;
+
+while(guess != answer){
+
+guess = prompt("Guess a number, 1-100");
+
+if (guess == answer){
+alert("Correct!");
+}
+
+else if (guess > answer){
+    alert("Too high!")
+}
+
+else if (guess < answer){
+    alert("Too low!")
+}
+
+else{
+    alert("Bad input")
+};
+};
+};
---
+++ script.js
@@ -0,0 +1,35 @@
+/* Simple Guess Program */
+
+// define function guessGame
+function guessGame() {
+  // generate a random integer 1-100
+let guess = 0
+let answer = Math.floor(Math.random()*100+1);
+  // loop while guess is not equal to answer or to "q"
+while (guess != answer || guess != "q"){
+    // set guess equal to asking user to "Guess a number, 1-100"
+guess = prompt("Guess a number, 1-100! (or type q to quit")
+    // if guess equals "q" display "Quitter!"
+if (guess == "q"){
+    alert("Quitter!")
+}
+    // or if guess equals answer display "Correct!" 
+else if(guess == answer){
+    alert("Correct!")
+}
+    // or if guess is greater than answer display "Too high!" 
+else if(guess > answer){
+    alert("Too high!")
+}
+    // or if guess is less than answer display "Too low!" 
+else if(guess < answer){
+    alert("Too low!")
+}
+    // or say "Bad input"
+else{
+    alert("Bad input")
+};
+  // end the loop
+};
+// end the function
+};
