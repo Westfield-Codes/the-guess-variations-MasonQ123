6508fe96cbc79426c8b8f199093e6bca23b4f5fd
Mason Quinn <mason117167@student-schoolsofwestfield.org>
2025-10-02 18:07:06 +0000

    step 4 is finished

--- pseudocode/done.js
+++ pseudocode/done.js
@@ -92,3 +92,80 @@ while (guess != answer && guess != "q"){
         }
     }
 };
+
+/* 4-guess-play-again.js */
+function guessGame(){
+  var again = true;
+while (again == true) {
+  
+let guess = 0
+let answer = Math.floor(Math.random()*100+1);
+
+while (guess != answer && guess != "q"){
+    guess = prompt("Guess a number, 1-100! (or type q to quit");
+        if (guess == "q"){
+            alert("Quitter!")
+        }
+        else if(guess == answer){
+            alert("Correct!")
+        }
+        else if(guess > answer){
+            alert("Too high!")
+        }
+        else if(guess < answer){
+            alert("Too low!")
+        }
+        else{
+            alert("Bad input")
+        }
+    }
+     again = confirm("Would you like to play again?")
+    if (again == false){
+        alert("Thanks for playing!")
+    }
+}
+}
+
+/* 4.5-guess-play-again.js */
+function guessGame (){
+
+  
+var again = true;
+while (again == true) {
+  guess = 0;
+  tries = 0;
+  answer = Math.floor(Math.random()*100+1);
+  
+while (guess != answer && guess != "q" && tries < 10){ {
+
+  guess = prompt("Guess a number, 1-100 or type q to quit");
+  tries += 1;
+if (guess == "q"){
+  alert("Quitter!")
+}
+else if (tries >= 10){
+  alert("Sorry, you've used all your tries. The answer was " + answer);
+}
+  else if (guess == answer){
+    alert("Correct in " + tries + " tries!");
+  }
+
+  else if (guess > answer){
+      alert("Too high!")
+  }
+
+  else if (guess < answer){
+      alert("Too low!")
+  }
+
+  else{
+      alert("Bad Input!")
+  }
+}
+}
+again = confirm("Would you like to play again?")
+if (again == false){ 
+  alert("Thanks for playing!") 
+}
+}
+}
